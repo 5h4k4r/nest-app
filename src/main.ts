ecaa460bfd8598ee78be9190b47dd816969cc59f
5h4k4r <shakar.halabjai@yahoo.com>
2022-09-20 15:30:34 +0300

    Setup swagger in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Posts example')
+    .setDescription('The posts API description')
+    .setVersion('1.0')
+    .addTag('posts')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(configService.get<number>('PORT') || 3000);
   Logger.log('Listening on port: ' + configService.get('PORT'));
 }
