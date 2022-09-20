d177c3dc7f02f88b4ad09a780ad75f2c8c562a51
5h4k4r <shakar.halabjai@yahoo.com>
2022-09-20 15:33:44 +0300

    Update app controller setup swagger for hello world route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
+  @ApiOperation({ summary: 'Get hello world response' })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    type: String,
+  })
   @Get()
   getHello(): any {
     return this.appService.getHello();
